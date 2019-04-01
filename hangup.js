a3b79c57d0e8af8274a983fdbdaa0d07cbc91437
Alan Gutierrez <alan@prettyrobots.com>
2019-04-01 14:30:19 -0400

    Log socket disconnection error.
    Log a socket disconnection error according to the revised logger
    interface in Procession's socket.

--- hangup.js
+++ hangup.js
@@ -1,5 +1,5 @@
 var logger = require('prolific.logger').createLogger('olio')
 
-module.exports = function (entry) {
-    logger.error(entry.label, entry)
+module.exports = function (error) {
+    logger.error('hangup', { stack: error.stack })
 }
