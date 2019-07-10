48e24bdc499af54642f15609e7a09ae252dc03ec
Alan Gutierrez <alan@prettyrobots.com>
2019-07-10 00:14:33 -0500

    Convert Sender to ES6.

--- sender.js
+++ sender.js
@@ -1,17 +1,19 @@
-var Keyify = require('keyify')
-var fnv = require('hash.fnv')
+const Keyify = require('keyify')
+const fnv = require('hash.fnv')
 
-function Sender (receivers, paths, count) {
-    this.processes = []
-    for (var i = 0; i < count; i++) {
-        this.processes.push({ conduit: receivers[i], path: paths[i], index: i })
+class Sender {
+    constructor (receivers, paths, count) {
+        this.processes = []
+        for (var i = 0; i < count; i++) {
+            this.processes.push({ conduit: receivers[i], path: paths[i], index: i })
+        }
+        this.count = count
     }
-    this.count = count
-}
 
-Sender.prototype.hash = function (key) {
-    var buffer = Buffer.from(Keyify.stringify(key))
-    return this.processes[fnv(0, buffer, 0, buffer.length) % this.count]
+    hash (key) {
+        const buffer = Buffer.from(Keyify.stringify(key))
+        return this.processes[fnv(0, buffer, 0, buffer.length) % this.count]
+    }
 }
 
 module.exports = Sender
