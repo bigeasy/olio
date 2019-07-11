31fc8da051cd300d97dfc4a5562cd132827556d3
Alan Gutierrez <alan@prettyrobots.com>
2019-07-10 23:38:22 -0500

    Sketch of new child construct.

--- test/run.bin.js
+++ test/run.bin.js
@@ -1,47 +1,8 @@
-var cadence = require('cadence')
-var Conduit = require('conduit/conduit')
-
-function Run (olio) {
-    this._olio = olio
-}
-
-Run.prototype.connect = cadence(function (async, destructible, inbox, outbox) {
-    destructible.durable('conduit', Conduit, inbox, outbox, cadence(function (async, request, inbox, outbox) {
-        return [ 1 ]
-    }), async())
-})
-
-Run.prototype.message = cadence(function (async, envelope) {
-    if (envelope.body.method == 'send') {
-        this._olio.broadcast('serve', { method: 'broadcast', sequence: envelope.body.sequence }, async())
-    }
-})
-
-Run.prototype.reconfigure = cadence(function () {
-    // TODO Someday.
-})
-
-// TODO No, we do not wait for all message to complete before exit, we just
-// exit, that's where we're at. Or maybe, it is always the client that hangs up
-// on the server and we're bi-directional then all the hangups will wind down a
-// multiplexer.
-
-// TODO Looks like you are getting ever more liberal with your use of
-// `Destructible`. The wrapper will need to know how to handle incoming
-// requests, so it needs a `receiver` function. If the child is going to call
-// itself, then it needs to wait for the wrapper to get the `receiver` before it
-// can create a sender on itself. We kind of had this apparent when Olio was
-// constructed with a listener builder as an argument if it was going to be a
-// server. Could pass in a builder.
-module.exports = cadence(function (async, destructible, olio) {
-    olio.on('application:request', function (message)  {
+module.exports = function (destructible, olio) {
+    olio.on('application:request', message => {
         olio.broadcast('serve', 'application:response', message)
     })
-    destructible.destruct.wait(function () {
-        console.log('YES DESTRUCTING!!!')
-    })
-    console.log('RUN CALLED')
-    destructible.destruct.wait(function () {
+    destructible.destruct(() => {
         // This is an example of a test that I know is working only through
         // coverage. I'd have to add instrumentation to know it worked, also
         // maybe print my TAP test `ok` directly to standard out here.
@@ -49,14 +10,11 @@ module.exports = cadence(function (async, destructible, olio) {
             olio.broadcast('serve', 'application:dropped', {})
             olio.send('serve', 0, 'application:dropped', {})
             olio.send('serve', 0, 'application:dropped', {}, {
-                destroy: function () {}
+                destroy: () => {}
             })
         })
     })
-    destructible.destruct.wait(function () { console.log('RUN DESTROYED') })
-    return new Run(olio)
-})
-
-process.on('exit', function () {
-    console.log('yes exit')
-})
+    olio.connect((header, inbox, outbox) => {
+        return 1
+    })
+}
--- test/serve.bin.js
+++ test/serve.bin.js
@@ -1,80 +1,15 @@
-var cadence = require('cadence')
-var http = require('http')
-var delta = require('delta')
-var Conduit = require('conduit/conduit')
-var Signal = require('signal')
-
-function Serve (olio) {
-    this._olio = olio
-    this.got = new Signal
+module.exports = async function (destructible, olio) {
+    const once = require('prosective/once')
+    const sender = await olio.sender('run')
+    const fastify = require('fastify')()
+    fastify.get('/worker/:id/conduit', async request => {
+        return await sender.hash(request.params.id).request({ value: 1 })
+    })
+    fastify.get('/worker/:index/ipc', async request => {
+        olio.send('run', +request.params.index, 'application:request', 1)
+        const [ result ] = await once(olio, 'application:response')
+        return result
+    })
+    await fastify.listen(8080)
+    destructible.destruct(() => fastify.close())
 }
-
-module.exports = cadence(function (async, destructible, olio) {
-    destructible.destruct.wait(function () { console.log('SERVE DESTROYED') })
-    var logger = require('prolific.logger').createLogger('olio.http')
-    var Reactor = require('reactor')
-    var Caller = require('conduit/caller')
-    var serve = new Serve
-    olio.on('application:response', function (message) {
-        serve.got.unlatch(null, message)
-    })
-    async(function () {
-        console.log('WILL CREATE SENDERS')
-        olio.sender('run', cadence(function (async, destructible, inbox, outbox) {
-            destructible.durable('conduit', Conduit, inbox, outbox, null, async())
-        }), async())
-    }, function (caller) {
-    console.log('got CALLER!!!!')
-        var reactor = new Reactor({
-            sequence: 0,
-            conduit: cadence(function (async, request, index) {
-                async(function () {
-                    caller.hash(String(index)).conduit.connect({
-                        url: request.url,
-                        body: request.body
-                    }).inbox.dequeue(async())
-                }, function (response) {
-                    return [ 200, { 'content-type': 'application/json' }, response ]
-                })
-            }),
-            message: cadence(function (async, request, index) {
-                async(function () {
-                console.log('ipc called!!!!!')
-                    var sequence = this.sequence++
-                    async(function () {
-                        olio.send('run', +index, 'application:request', sequence)
-                    }, function () {
-                        console.log('waiting')
-                        serve.got.wait(async())
-                    }, function (got) {
-                        console.log('>>>', got)
-                        return got
-                    })
-                }, function (response) {
-                    return [ 200, { 'content-type': 'application/json' }, response ]
-                })
-            }),
-            index: cadence(function (async) {
-                return 'Hello, World!\n'
-            })
-        }, function (dispatcher) {
-            dispatcher.dispatch('GET /', 'index')
-            dispatcher.dispatch('POST /worker/:id/conduit', 'conduit')
-            dispatcher.dispatch('POST /worker/:id/ipc', 'message')
-            dispatcher.logger = function (entry) {
-                if (entry.error) {
-                    console.log(entry.error.stack)
-                }
-            }
-        })
-        var server = http.createServer(reactor.middleware)
-        async(function () {
-            destructible.destruct.wait(server, 'close')
-            server.listen(8888)
-            delta(async()).ee(server).on('listening')
-        }, function () {
-            delta(destructible.durable('http')).ee(server).on('close')
-            return serve
-        })
-    })
-})
